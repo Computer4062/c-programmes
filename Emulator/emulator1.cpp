9ea12f17c0cac4256735c0f1ccc095df78de8d9a
Mihan <87608466+Computer4062@users.noreply.github.com>
2023-05-20 13:27:12 +0400

    Update emulator1.cpp

--- Emulator/emulator1.cpp
+++ Emulator/emulator1.cpp
@@ -16,6 +16,16 @@ typedef struct {
     uint32_t scale_factor; //amount to scale the CHIP8  pixel
 } config_t;
 
+typedef enum {
+    QUIT = 0,
+    RUNNING,
+    PAUSED
+} emulator_state_t;
+
+typedef struct {
+    emulator_state_t state;
+} chip8_t;
+
 bool init_sdl(sdl_t* sdl, const config_t* config) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
         SDL_Log("could not initialize SDL subsystems! %s\n", SDL_GetError());
@@ -65,6 +75,10 @@ bool set_config_from_args(config_t* config, const int argc, char **argv) {
     return true;
 }
 
+bool init_chip8(chip8_t *chip8){
+    chip8->state = RUNNING; //Default state
+    return true;
+}
 
 void final_cleanup(sdl_t* sdl) {
     SDL_DestroyRenderer(sdl->renderer);
@@ -73,6 +87,7 @@ void final_cleanup(sdl_t* sdl) {
 }
 
 void clear_screen(const config_t config, sdl_t* sdl){
+    //yellow color
     const uint8_t r = (config.bg_color >> 24) & 0xFF;
     const uint8_t g = (config.bg_color >> 16) & 0xFF;
     const uint8_t b = (config.bg_color >> 8) & 0xFF;
@@ -87,38 +102,52 @@ void update_screen(sdl_t *sdl){
     SDL_RenderPresent(sdl->renderer);
 }
 
+void handle_input(chip8_t *chip8){
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event)){
+        switch(event.type){
+            case SDL_QUIT:
+                chip8->state = QUIT;
+                return;
+
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym){
+                    case SDLK_ESCAPE:
+                        //exit the window and progrmme
+                        chip8->state = QUIT;
+
+                    default:
+                        break;
+                }
+                break;
+
+            case SDL_KEYUP:
+                break;
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
 
     config_t config = { 0 };
-    if (!set_config_from_args(&config, argc, argv))
-        exit(EXIT_FAILURE);
+    if (!set_config_from_args(&config, argc, argv)) exit(EXIT_FAILURE);
 
     sdl_t sdl = { 0 };
-    if (!init_sdl(&sdl, &config))
-        exit(EXIT_FAILURE);
+    if (!init_sdl(&sdl, &config)) exit(EXIT_FAILURE);
+
+    chip8_t chip8;
+    if(!init_chip8(&chip8)) exit(EXIT_FAILURE);
 
     clear_screen(config, &sdl);
+    while (chip8.state != QUIT) {
+        handle_input(&chip8);
 
-    bool quit = false;
-    SDL_Event event;
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) 
-                quit = true;
-
-            if(event.type == SDL_KEYDOWN)
-                quit = true;
-
-        
         SDL_Delay(16);
         update_screen(&sdl);
     }
-
-    // Perform any other game logic or updates here
-}
-
 
     final_cleanup(&sdl);
 
